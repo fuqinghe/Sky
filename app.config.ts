5d0f9233cf0d9858cb2a6398d035d4f12a450886
Tloml-Starry <109225278+fuqinghe@users.noreply.github.com>
2023-05-30 22:58:42 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,4 @@
-import path from 'path'
 /**插件名*/
 export const AppName = 'Sky'
 /*插件绝对路径 */
-export const MyDirPath = `${path.resolve().replace(/\\/g, '/')}/plugins/${AppName}`
+export const MyDirPath = `${process.cwd().replace(/\\/g, '/')}/plugins/${AppName}`
